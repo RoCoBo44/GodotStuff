65836ef423286e9bb69955515a2d813621a3f693
RoCobo <39014389+RoCobo@users.noreply.github.com>
2019-09-13 19:01:28 -0300

    added circle stop + fixed arch

--- Testing/KinematicBody2D.gd
+++ Testing/KinematicBody2D.gd
@@ -1,20 +1,22 @@
 extends KinematicBody2D
-export (int) var speed = 200
+export (int) var speed = 100
 
+var maxIda 
 var velocity = Vector2()
 var auto = false
 var movimientos = []
 var movActual = 0
 export var movMaximo = 80
 var stop = false
+var ida 
 
 func get_input():
-	
+	velocity = Vector2()	
+
 	if Input.is_key_pressed(KEY_K):
 		auto = !auto
 	
 	if (!auto):
-	    velocity = Vector2()
 	    if Input.is_action_pressed('ui_right'):
 	        velocity.x += 1
 	    if Input.is_action_pressed('ui_left'):
@@ -24,29 +26,33 @@ func get_input():
 	    if Input.is_action_pressed('ui_up'):
 	        velocity.y -= 1
 	else:
-		velocity = Vector2()
-		velocity.x = movimientos[movActual]
-		velocity.y = movimientos[movActual+1]
-		movActual = movActual+2
-		if (movActual >= movMaximo):
-			movActual=0
+		if (ida > 0):
+			ida-=1
+			velocity.x = movimientos[movActual]
+			velocity.y = movimientos[movActual+1]
+			if ida == 0:
+				ida = maxIda
+				movActual = movActual+2
+				if (movActual >= movMaximo):
+					movActual=0
 		
 	velocity = velocity.normalized() * speed
 func _ready():
+	randomize()
 	_cargar_movimientos()
+	maxIda = int(rand_range(50,200))
+	ida = maxIda
 	
 func _cargar_movimientos():
-	
 	for i in range(0,movMaximo):
 		movimientos.append(rand_range(-1,1))
 
 func _physics_process(delta):
-	
 	get_input()
 	if not stop:
 		velocity = move_and_slide(velocity)
 
-
-
-func _on_Base_body_exited(body):
-	stop = true
+func _on_Area2D_area_exited(area):
+	if area.get_name() == 'Base':
+		stop = true
+		print('bye' + area.get_name())
--- Testing/Kinetic Move.gd
+++ Testing/Kinetic Move.gd
@@ -4,8 +4,29 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+export var cantidad = 4
+var radius = 240
+var angle_step
+onready var bot = preload("res://Body/Bot.tscn")
+onready var botContainer = get_node("botsContainer")
+var center 
+var screen_size = OS.get_screen_size()
+var window_size = OS.get_window_size()
+
 func _ready():
-	pass
+	OS.set_window_position(screen_size*0.5 - window_size*0.5)
+	center = window_size/2
+	angle_step = 2.0*PI / cantidad
+	var angle = 0
+	var tam = 4
+	for i in range(0,cantidad):
+		var direction = Vector2(cos(angle), sin(angle))
+		var node = bot.instance()
+		node.set_name("boti{0}".format(i))
+		node.scale = Vector2(tam,tam)
+		node.position = center + direction * radius
+		add_child(node)
+		angle += angle_step
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
