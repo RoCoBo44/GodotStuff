d27c5c30fa3ae51a5ad0412bd71792c1470c739c
RoCobo <39014389+RoCobo@users.noreply.github.com>
2019-09-23 22:35:20 -0300

    Added textures + Stuff

--- Testing/Body/vagabundo.gd
+++ Testing/Body/vagabundo.gd
@@ -23,10 +23,35 @@ func interaction(otroBot):
 			var porsVag = 1
 			if money > 0:
 				porsVag = m/money
+			var auxScale = scale
 			if (randf() < (BaseChance * weightBase + porsUser * weigntPorsUser + porsVag * weightPorsVag)/(weightPorsVag +weigntPorsUser + weightBase)):
-				otroBot.receive_money(int(rand_range(1 ,money)))#change min value
+			#if true:
+				var moneyToAward = int(rand_range(1 + 20 *porsUser + 20 * porsVag ,money)) #change min value
+				if moneyToAward > money:
+					moneyToAward = money
+				money -= moneyToAward
+				otroBot.receive_money(moneyToAward)
+				animate(Color(2, 0.8, 0))
 			else:
+				animate(Color(1, 0.7, 0.4))
 				money+=m
 				otroBot.reduce_money(m)
 			label.set_text(String(money))
+			scale = auxScale
 
+func animate(c):
+	var auxScale = scale
+	$Tween.interpolate_property(self, "scale", auxScale, Vector2(5,5), 0.2, Tween.TRANS_BOUNCE  , Tween.EASE_IN_OUT)
+	$Tween.start()
+	yield($Tween, "tween_completed")
+	$Tween.interpolate_property($Sprite, "modulate", Color(1,0.4,0), c, 0.3, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	$Tween.start()
+	yield($Tween, "tween_completed")
+	$Tween.interpolate_property(self, "scale", Vector2(5,5), auxScale , 0.2, Tween.TRANS_BOUNCE  , Tween.EASE_IN_OUT)
+	$Tween.start()
+	yield($Tween, "tween_completed")
+	$Tween.interpolate_property($Sprite, "modulate", c,  Color(1 , 0.4 , 0), 0.3, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	$Tween.start()
+	yield($Tween, "tween_completed")
+	$Sprite.modulate = Color(1,0.4,0)
+	scale = auxScale
